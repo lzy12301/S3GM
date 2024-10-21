f788d6bf2ce582a0bc5cc51151afeb78c93bf891
lzy12301 <140318352+lzy12301@users.noreply.github.com>
2024-10-21 12:01:09 +0800

    Update run_s3gm.sh

--- run_s3gm.sh
+++ run_s3gm.sh
@@ -1,5 +1,5 @@
 # For KSE
-# CUDA_VISIBLE_DEVICES=0 python train.py --data kse --version vx --dims 1 --epochs 300 --batch_size 64 --train_split 0.9 --num_components 1 --num_conditions 1 --attn_resolutions 16 --ch_mult 1 2 4 8 --data_location ./data/kse/KSE_train.npy --verbose 1
+# CUDA_VISIBLE_DEVICES=0 python train.py --data kse --version vx --dims 1 --epochs 300 --batch_size 50 --train_split 0.9 --num_components 1 --num_conditions 1 --attn_resolutions 16 --ch_mult 1 2 4 8 --data_location ./data/kse/KSE_train.npy --verbose 1
 
 # For Kolmogorov flow
 # CUDA_VISIBLE_DEVICES=0 python train.py --data kol --version vx --dims 2 --epochs 300 --batch_size 32 --train_split 0.8 --num_components 2 --num_conditions 2 --attn_resolutions 8 16 --ch_mult 1 2 4 8 --data_location ./data/kolmogorov/kolmogorov_flow_train.npy --verbose 1
